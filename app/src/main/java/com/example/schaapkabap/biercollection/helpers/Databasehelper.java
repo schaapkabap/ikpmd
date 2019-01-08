b9d5853d2c187a022c7d2417382ce604bd56f9e1
Alexander <alexiaan.vd@hotmail.com>
2019-01-08 13:16:26 +0100

    New helpers

---
+++ app/src/main/java/com/example/schaapkabap/biercollection/helpers/Databasehelper.java
@@ -0,0 +1,14 @@
+package com.example.schaapkabap.biercollection.helpers;
+
+// Databasehelper for connection with firebase
+public class Databasehelper {
+
+    private String name;
+    private String password;
+    private String ref;
+
+    public void getConnection(){
+
+    }
+
+}
